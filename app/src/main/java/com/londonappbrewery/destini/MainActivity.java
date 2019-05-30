40a33361e968ea1c39e577f9b2ed857a46a40f67
Gabriel Oliveira <g-araujo94@hotmail.com>
2019-05-30 15:10:52 -0300

    A terminar

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,6 +2,7 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -25,10 +26,10 @@ public class MainActivity extends AppCompatActivity {
 
     Answer T1_1 = new Answer(R.string.T1_Ans1);
     Answer T1_2 = new Answer(R.string.T1_Ans2);
-    Answer T3 = new Answer(R.string.T2_Ans1);
-    Answer T4 = new Answer(R.string.T2_Ans2);
-    Answer T5 = new Answer(R.string.T3_Ans1);
-    Answer T6 = new Answer(R.string.T3_Ans2);
+    Answer T2_1 = new Answer(R.string.T2_Ans1);
+    Answer T2_2 = new Answer(R.string.T2_Ans2);
+    Answer T3_1 = new Answer(R.string.T3_Ans1);
+    Answer T3_2= new Answer(R.string.T3_Ans2);
 
     //indice corrente da historia
     private Story mStorySelected;
@@ -43,12 +44,38 @@ public class MainActivity extends AppCompatActivity {
 
         //TODO:faça o mapeamento da história
 
-        mT1.setAnswerBottom(T1_2);
         mT1.setAnswerTop(T1_1);
+        mT1.setAnswerBottom(T1_2);
+
+        T1_1.setChildStory(mT3);
+        T1_2.setChildStory(mT2);
+
+        T3_1.setChildStory(mT6);
+        T3_2.setChildStory(mT5);
+
+        mT2.setAnswerTop(T2_1);
+        mT2.setAnswerTop(T2_2);
+
+        T2_1.setChildStory(mT3);
+        T2_2.setChildStory(mT4);
+
+        mT3.setAnswerTop(T3_1);
+        mT3.setAnswerBottom(T3_2);
+
+        T3_1.setChildStory(mT6);
+        T3_2.setChildStory(mT5);
+    }
 
         // TODO: Coloque o evento do click do botão, caso precise colocar a visibilidade no botão invisivel utilize a função
         // do botão setVisibility(View.GONE):
 
+        public void butT(View v){
+            Log.d();
+
+    }
+
+        public void butB(View v){
+            Log.d();
 
     }
 
